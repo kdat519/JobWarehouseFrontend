2aae06fa941445c31ececfb83615e550c2a5db88
Pham Khac Dat <kdat519@gmail.com>
2021-11-27 10:20:04 +0700

    Add Admin Api

---
+++ src/api/adminApi.js
@@ -0,0 +1,20 @@
+import axiosClient from "./axiosClient"
+
+const adminApi = {
+    getUserList: (params) => {
+        const url = "admin/user";
+        return axiosClient.get(url, { params });
+    },
+
+    getUser: (id) => {
+        const url = `auth/user/${id}`;
+        return axiosClient.get(url);
+    },
+
+    banUser: (id) => {
+        const url = `auth/user/${id}`;
+        return axiosClient.post(url);
+    }
+}
+
+export default adminApi;
